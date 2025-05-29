2c46a8eb8c29d7180cdc0ea9336dbaa60de00b8a
AmulyaVudutha <amulyavudutha07@gmail.com>
2025-05-29 21:48:09 +0530

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,8 @@
-export let cart =JSON.parse(localStorage.getItem('cart'));
+export let cart;
+loadFromStorage();
+
+export function loadFromStorage(){
+  cart =JSON.parse(localStorage.getItem('cart'));
 
 if(!cart){
   cart=[{
@@ -13,7 +17,7 @@ if(!cart){
     deliveryOptionId:'2'
 }];
 }
-
+}
 function saveToStorage(){
   localStorage.setItem('cart',JSON.stringify(cart))
 }
